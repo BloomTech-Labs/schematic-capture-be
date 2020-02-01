df112a87708025fb01bdac7534e83faf667bb559
Nattajohn Rojanasupya <n.rojanasupya@gmail.comm>
2020-01-31 23:23:12 -0800

    adds middleware for redirecting requests when handling google auth flow

--- api/middleware/index.js
+++ api/middleware/index.js
@@ -1,3 +1,4 @@
 exports.FBauth = require("./FBauth");
 exports.validateInvitation = require("./validateInvitation");
+exports.checkAccountExists = require("./checkAccountExists");
 exports.validateGoogleSignIn = require("./validateGoogleSignIn");
--- api/middleware/validateGoogleSignIn.js
+++ api/middleware/validateGoogleSignIn.js
@@ -1,24 +1,46 @@
+const url = require("url");
 const { admin } = require("../../utils/firebase");
 
 module.exports = (req, res, next) => {
-    const { token } = req.body;
-    if (token) {
-        admin
-            .auth()
-            .verifyIdToken(token)
-            .then(decodedToken => {
-                req.email = decodedToken.email;
-                req.uid = decodedToken.uid;
-                req.token = token;
+  const { token } = req.body;
+  if (token) {
+    admin
+      .auth()
+      .verifyIdToken(token)
+      .then(decodedToken => {
+        const query = {
+          email: decodedToken.email,
+          uid: decodedToken.uid,
+          idToken: token
+        };
 
-                console.log(req.uid);
-                return next();
+        if (req.invite_token) {
+          res.redirect(
+            url.format({
+              pathname: "/api/auth/google/create",
+              query: {
+                ...query,
+                ...req.body,
+                inviteToken: req.invite_token
+              }
             })
-            .catch(error => {
-                console.error("Error while verifying token ", error);
-                return res.status(403).json({ error: "I'm here.." });
-            });
-    } else {
-        return next();
-    }
+          );
+        } else {
+          res.redirect(
+            url.format({
+              pathname: "/api/auth/google/signin",
+              query
+            })
+          );
+        }
+      })
+      .catch(error =>
+        res.status(403).json({
+          error: error.message,
+          step: "validateGoogleSignIn"
+        })
+      );
+  } else {
+    return next();
+  }
 };
