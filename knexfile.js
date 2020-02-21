c9af0860c4957dee44be5eb48a5fdcbbef8c1623
Nattajohn Rojanasupya <n.rojanasupya@gmail.comm>
2020-02-20 20:50:33 -0800

    adds testing configs

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,26 @@
 require("dotenv").config();
 
 module.exports = {
+    test: {
+        client: "pg",
+        connection: {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE
+        },
+        pool: {
+            min: 2,
+            max: 20
+        },
+        migrations: {
+            directory: "./data/migrations"
+        },
+        seeds: {
+            directory: "./data/seeds"
+        }
+    },
     development: {
         client: "pg",
         connection: {
