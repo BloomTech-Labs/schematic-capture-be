7b70af78328245d6467c03b6bbaf83f72c518330
Brad <brad@viziworx.com>
2020-02-27 15:38:20 -0500

    fixes the update method to return findBy

--- data/models/BaseModel.js
+++ data/models/BaseModel.js
@@ -1,69 +1,67 @@
-const db = require('../dbConfig');
-
+const db = require("../dbConfig");
 
 class BaseModel {
-  constructor(table) {
-    this.table = table;
-  }
+    constructor(table) {
+        this.table = table;
+    }
 
-  _find() {
-    return db(this.table);
-  }
+    _find() {
+        return db(this.table);
+    }
 
-  _findBy(filter) {
-    return db(this.table).where(filter);
-  }
+    _findBy(filter) {
+        return db(this.table).where(filter);
+    }
 
-  _findByMultiple(filter, by) {
-    return db(this.table).whereIn(filter, by);
-  }
+    _findByMultiple(filter, by) {
+        return db(this.table).whereIn(filter, by);
+    }
 
-  _add(data, filter=null) {
-    return db(this.table)
-      .insert(data, 'id')
-      .then(ids => {
-        const [id] = ids;
-        return this.findBy(filter || { id });
-      });
-  }
+    _add(data, filter = null) {
+        return db(this.table)
+            .insert(data, "id")
+            .then(ids => {
+                const [id] = ids;
+                return this.findBy(filter || { id });
+            });
+    }
 
-  _update(filter, changes) {
-    return db(this.table).findBy(filter)
-      .update(changes, 'id')
-      .then(ids => {
-        const [id] = ids;
-        return this.findBy(filter);
-      })
-  }
+    _update(filter, changes) {
+        return this.findBy(filter)
+            .update(changes, "id")
+            .then(ids => {
+                const [id] = ids;
+                return this.findBy(filter);
+            });
+    }
 
-  _remove(filter) {
-    return this.findBy(filter)
-      .delete('id')
-  }
+    _remove(filter) {
+        return this.findBy(filter).delete("id");
+    }
 
-  find() {
-    return this._find();
-  }
+    find() {
+        return this._find();
+    }
 
-  findBy(filter) {
-    return this._findBy(filter);
-  }
+    findBy(filter) {
+        return this._findBy(filter);
+    }
 
-  findByMultiple(filter, by) {
-    return this._findByMultiple(filter, by);
-  }
+    findByMultiple(filter, by) {
+        return this._findByMultiple(filter, by);
+    }
 
-  add(data, filter=null) {
-    return this._add(data, filter);
-  }
+    add(data, filter = null) {
+        return this._add(data, filter);
+    }
 
-  update(filter, changes) {
-    return this._update(filter, changes);
-  }
+    update(filter, changes) {
+        return this._update(filter, changes);
+    }
 
-  remove(filter) {
-    return this._remove(filter);
-  }
+    remove(filter) {
+        return this._remove(filter);
+    }
 }
 
 module.exports = BaseModel;
