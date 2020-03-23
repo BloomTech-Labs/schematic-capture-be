d7a81fa76a5184003630ee4d61578ed003b12bf5
adastraz <tylergosox@gmail.com>
2020-03-23 13:44:35 -0400

    make sure to pull changes from table-changes these allow for put to assign user to all project jobs

--- api/routers/projects.js
+++ api/routers/projects.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const { Projects, Jobsheets, Components } = require("../../data/models");
 const getUserInfo = require("../middleware/users/getUserInfo");
 const checkIfProjectExists = require("../middleware/projects/checkIfProjectExists")
+const checkBodyForAssigned = require("../middleware/projects/checkBodyForAssigned")
+
 
 router.get("/:id/jobsheets", checkIfProjectExists, async (req, res) => {
     const { id } = req.params;
@@ -57,5 +59,18 @@ router.get("/:id/jobsheets", checkIfProjectExists, async (req, res) => {
             .json({ error: error.message, step: "/:id/jobsheets" });
     }
 });
+
+router.put('/:id/jobsheets', checkIfProjectExists, checkBodyForAssigned, async (req, res) => {
+    const { id } = req.params
+
+    Jobsheets.update({ project_id: id}, req.body)
+    .then(updatedJob => {
+        res.status(201).json(updatedJob)
+    })
+    .catch(err => {
+        err.status(404)
+        .json({ error: error.message, step: "/:id/jobsheets" })
+    })
+})
 
 module.exports = router;
