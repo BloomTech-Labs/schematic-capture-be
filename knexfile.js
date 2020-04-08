4ec4ec32fe4a22245e409b631c8890f958b827ed
Tyler Thompson <57100235+adastraz@users.noreply.github.com>
2020-04-08 16:00:12 -0400

    process.RDS_DB_NAME

--- knexfile.js
+++ knexfile.js
@@ -38,7 +38,8 @@ module.exports = {
             host     : process.env.RDS_HOSTNAME,
             user     : process.env.RDS_USERNAME,
             password : process.env.RDS_PASSWORD,
-            port     : process.env.RDS_PORT
+            port     : process.env.RDS_PORT,
+            database : process.env.RDS_DB_NAME
         },
         pool: {
             min: 2,
