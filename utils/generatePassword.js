1486616eb57f1286c2b8c451a2dd56f46672a68f
Vincent Sanders <vincesanders@hotmail.com>
2020-04-13 20:57:57 -0500

    Added generatePassword function to automatically generate secure passwords.

---
+++ utils/generatePassword.js
@@ -0,0 +1,11 @@
+const generatePassword = (passwordLength) => {
+    const length = passwordLength,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*-+=?",
+        password = "";
+    for (let i = 0, n = charset.length; i < length; ++i) {
+        password += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return password;
+}
+
+module.exports = { generatePassword };
