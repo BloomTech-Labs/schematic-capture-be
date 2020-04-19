22aabb06b2be99a84b91ba483279b4bbc7869a21
Vincent Sanders <vincesanders@hotmail.com>
2020-04-18 22:20:30 -0500

    Refactored sendEmailInvite middleware.

--- api/middleware/auth/sendEmailInvite.js
+++ api/middleware/auth/sendEmailInvite.js
@@ -1,9 +1,7 @@
 const axios = require('axios');
 
 const config = {
-    headers: {
-        Authorization: `Bearer ${process.env.SG_API_KEY}`
-    }
+    headers: { Authorization: `Bearer ${process.env.SG_API_KEY}` }
 };
 
 module.exports = (req, res, next) => {
@@ -12,22 +10,17 @@ module.exports = (req, res, next) => {
     //this method of attaching token to the url should be eventually changed.
     const registrationUrl = `https://schematiccapture.com/firstlogin/${req.token}`;
     const data = {
-        personalizations: [
-            {
-                to: [{ email: req.body.email, name: req.body.name }],
-                dynamic_template_data: { registrationUrl, token: req.token }
-            }
-        ],
-        from: {
-            email: "invitation@schematiccapture.com",
-            name: "Schematic Capture"
-        },
+        personalizations: [{
+            to: [{ email: req.body.email, name: req.body.name }],
+            dynamic_template_data: { registrationUrl, token: req.token }
+        }],
+        from: { email: "invitation@schematiccapture.com", name: "Schematic Capture" },
         template_id: process.env.SG_TEMPLATE_ID
     };
     //send email
     axios.post("https://api.sendgrid.com/v3/mail/send", data, config).then(() => {
         next();
     }).catch(error => {
-        res.status(500).json({ error: err, message: 'Failed to send email to user.', step: 'sendEmailInvite middleware' });
+        res.status(500).json({ error: error, message: 'Failed to send email to user.', step: 'sendEmailInvite middleware' });
     });
 }
