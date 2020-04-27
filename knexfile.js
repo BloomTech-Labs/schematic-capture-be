c07a8feb810a953732ddc5a606925f34f34f8e24
Vincent Sanders <vincesanders@hotmail.com>
2020-04-27 15:58:59 -0500

    Added knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+
+const postgresConfig = {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+        min: 2,
+        max: 20
+    },
+    migrations: {
+        directory: "./data/migrations"
+    },
+    seeds: {
+        directory: "./data/seeds"
+    }
+}
+
+module.exports = {
+    test: {
+        client: 'sqlite3',
+        connection: {
+            filename: './data/test.db3'
+        },
+        useNullAsDefault: true,
+        migrations: {
+            directory: './data/migrations',
+        },
+        seeds: {
+            directory: './data/seeds',
+        }
+    },
+    staging: postgresConfig,
+    production: postgresConfig,
+    development: {
+        client: 'sqlite3',
+        connection: {
+            filename: './data/schematic_capture.db3'
+        },
+        useNullAsDefault: true,
+        migrations: {
+            directory: './data/migrations',
+        },
+        seeds: {
+            directory: './data/seeds',
+        }
+    }
+};
