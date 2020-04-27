e0c295ac9099aa10601fc988bb93a6409328d581
Vincent Sanders <38077240+vincesanders@users.noreply.github.com>
2020-04-27 17:13:31 -0500

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -35,7 +35,7 @@ module.exports = {
         connection: process.env.DATABASE_URL,
         pool: {
             min: 2,
-            max: 50
+            max: 10
         },
         migrations: {
             directory: "./data/migrations"
