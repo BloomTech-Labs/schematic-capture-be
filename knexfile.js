af0cc71488aa6725ffa345b6b43455dadb5332df
Vincent Sanders <vincesanders@hotmail.com>
2020-04-27 16:04:45 -0500

    Made some changes to knexfile.

--- knexfile.js
+++ knexfile.js
@@ -30,7 +30,20 @@ module.exports = {
         }
     },
     staging: postgresConfig,
-    production: postgresConfig,
+    production: {
+        client: "pg",
+        connection: process.env.DATABASE_URL,
+        pool: {
+            min: 2,
+            max: 20
+        },
+        migrations: {
+            directory: "./data/migrations"
+        },
+        seeds: {
+            directory: "./data/seeds"
+        }
+    },
     development: {
         client: 'sqlite3',
         connection: {
