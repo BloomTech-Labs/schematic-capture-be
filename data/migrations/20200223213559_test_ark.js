c10062f99fe42295b8c5cd565d3daeb104e7f9a7
Vincent Sanders <vincesanders@hotmail.com>
2020-04-27 21:15:22 -0500

    Unded migration changes.

--- data/migrations/20200223213559_test_ark.js
+++ data/migrations/20200223213559_test_ark.js
@@ -98,91 +98,17 @@ const contactsConfig = [
 ]
 
 exports.up = function(knex) {
-    return knex.schema.createTable("roles", table => {
-        table.increments();
-        table.string("name").notNullable();
-    })
-    .createTable("organizations", table => {
-        table.increments();
-        table.string("name").notNullable();
-        table.string("phone");
-        table.string("street");
-        table.string("city");
-        table.string("state");
-        table.string("zip");
-    })
-    .createTable("users", table => {
-        table.string("id").unique().primary();
-        table.integer("role_id").unsigned().references("id").inTable("roles").notNullable();
-        table.string("email").notNullable().unique();
-        table.string("first_name").notNullable();
-        table.string("last_name").notNullable();
-        table.string("phone");
-    })
-    .createTable('users_organizations', table => {
-        table.primary(['organization_id', 'user_email']);
-        table.string("user_email").references("email").inTable("users").notNullable();
-        table.integer("organization_id").unsigned().references("id").inTable("organizations").notNullable();
-    })
-    .createTable('invite_tokens', table => {
-        table.string('id').unique().primary();
-    })
-    .createTable("clients", table => {
-        table.increments();
-        table.integer("organization_id").unsigned().references("id").inTable("organizations").notNullable();
-        table.string("company_name").notNullable();
-        table.string("phone");
-        table.string("street");
-        table.string("city");
-        table.string('state');
-        table.string("zip");
-    })
-    .createTable("projects", table => {
-        table.increments();
-        table.integer("client_id").unsigned().references("id").inTable("clients").notNullable();
-        table.string("name").notNullable();
-    })
-    .createTable("jobsheets", table => {
-        table.increments();
-        table.timestamp('updated_at').defaultTo(new Date().toISOString()).notNullable();
-        table.string('status').defaultTo('Unassigned').notNullable();
-        table.string('user_email').references('email').inTable('users')
-        table.string('name').notNullable();
-        table.integer("project_id").unsigned().references("id").inTable("projects").notNullable();
-    })
-    .createTable("custom_fields", table => {
-        table.increments();
-        table.integer("jobsheet_id").unsigned().references("id").inTable("jobsheets").notNullable();
-        table.string("col_name").notNullable();
-    })
-    .createTable("components", table => {
-        table.increments();
-        table.integer("jobsheet_id").unsigned().references("id").inTable("jobsheets").notNullable();
-        table.string("component_id").notNullable();
-        table.string("rl_category");
-        table.string("rl_number");
-        table.string("descriptions");
-        table.string("manufacturer");
-        table.string("part_number");
-        table.string("stock_code");
-        table.string("component_application");
-        table.string("electrical_address");
-        table.string("reference_tag");
-        table.string("settings");
-        table.string("image");
-        table.string("resources");
-        table.string("cutsheet");
-        table.string("maintenance_video");
-        table.string("custom");
-    })
-    .createTable('contacts', table => {
-        table.increments();
-        table.integer('client_id').unsigned().references('id').inTable('clients').notNullable();
-        table.string('first_name').notNullable();
-        table.string('last_name').notNullable();
-        table.string('phone')
-        table.string('email')
-    });
+    return knex.schema.createTable("roles", table => createTableCallback(table, rolesConfig))
+        .createTable("organizations", table => createTableCallback(table, organizationsConfig))
+        .createTable("users", table => createTableCallback(table, userConfig))
+        .createTable('users_organizations', table => createTableCallback(table, usersOrganizationsConfig))
+        .createTable('invite_tokens', table => createTableCallback(table, inviteTokensConfig))
+        .createTable("clients", table => createTableCallback(table, clientsConfig))
+        .createTable("projects", table => createTableCallback(table, projectsConfig))
+        .createTable("jobsheets", table => createTableCallback(table, jobsheetsCofig))
+        .createTable("custom_fields", table => createTableCallback(table, customFieldsConfig))
+        .createTable("components", table => createTableCallback(table, componentsConfig))
+        .createTable('contacts', table => createTableCallback(table, contactsConfig));
 };
 
 exports.down = function(knex) {
