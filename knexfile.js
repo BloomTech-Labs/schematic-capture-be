9d50d6729b3bf7ca06ccd939d2cbc98b944b9b84
Vincent Sanders <vincesanders@hotmail.com>
2020-04-27 19:33:38 -0500

    back to cdc15f5.

--- knexfile.js
+++ knexfile.js
@@ -30,20 +30,7 @@ module.exports = {
         }
     },
     staging: postgresConfig,
-    production: {
-        client: "pg",
-        connection: process.env.DATABASE_URL,
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            directory: "./data/migrations"
-        },
-        seeds: {
-            directory: "./data/seeds"
-        }
-    },
+    production: postgresConfig,
     development: {
         client: 'sqlite3',
         connection: {
