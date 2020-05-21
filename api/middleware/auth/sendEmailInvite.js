6f7ba8607c01f22c6bbe524f0571d7337922ca0b
CeeDeeBee <cdbarnes98@gmail.com>
2020-05-21 19:35:25 -0400

    Added token validation in front of role auth middleware + added optional password param to generateToken

--- api/middleware/auth/sendEmailInvite.js
+++ api/middleware/auth/sendEmailInvite.js
@@ -1,26 +1,36 @@
-const axios = require('axios');
+const axios = require("axios");
 
 const config = {
-    headers: { Authorization: `Bearer ${process.env.SG_API_KEY}` }
+	headers: { Authorization: `Bearer ${process.env.SG_API_KEY}` },
 };
 
 module.exports = (req, res, next) => {
-    //send an email that contains a link to sign-in with the token in the url
-    //CANNOT register at any other endpoint. This is how we make sure people were invited
-    //this method of attaching token to the url should be eventually changed.
-    const registrationUrl = `https://schematiccapture.com/firstlogin/${req.token}`;
-    const data = {
-        personalizations: [{
-            to: [{ email: req.body.email, name: req.body.name }],
-            dynamic_template_data: { registrationUrl, token: req.token }
-        }],
-        from: { email: "invitation@schematiccapture.com", name: "Schematic Capture" },
-        template_id: process.env.SG_TEMPLATE_ID
-    };
-    //send email
-    axios.post("https://api.sendgrid.com/v3/mail/send", data, config).then(() => {
-        next();
-    }).catch(error => {
-        res.status(500).json({ error: error, message: 'Failed to send email to user.', step: 'sendEmailInvite middleware' });
-    });
-}
+	//send an email that contains a link to sign-in with the token in the url
+	//CANNOT register at any other endpoint. This is how we make sure people were invited
+	//this method of attaching token to the url should be eventually changed.
+	const registrationUrl = `https://schematiccapture.com/firstlogin/${req.token}`;
+	const data = {
+		personalizations: [
+			{
+				to: [{ email: req.body.email, name: req.body.name }],
+				dynamic_template_data: { registrationUrl, token: req.token },
+			},
+		],
+		from: {
+			email: "invitation@schematiccapture.com",
+			name: "Schematic Capture",
+		},
+		template_id: process.env.SG_TEMPLATE_ID,
+	};
+	//send email
+	axios
+		.post("https://api.sendgrid.com/v3/mail/send", data, config)
+		.then(() => next())
+		.catch((error) => {
+			res.status(500).json({
+				error: error,
+				message: "Failed to send email to user.",
+				step: "sendEmailInvite middleware",
+			});
+		});
+};
--- api/routers/users.js
+++ api/routers/users.js
@@ -1,15 +1,24 @@
 const router = require("express").Router();
 const { Users } = require("../../data/models");
-const dbToRes = require('../../utils/dbToRes');
-const superRoleIdAuth = require('../middleware/auth/superRoleIdAuth')
+const dbToRes = require("../../utils/dbToRes");
+const validateIdToken = require("../middleware/auth/validateIdToken");
+const superRoleIdAuth = require("../middleware/auth/superRoleIdAuth");
 
-router.get('/', superRoleIdAuth, (req, res) => {
-    Users.find().then(users => {
-        users = users.map(user => dbToRes(user));
-        res.status(200).json(users);
-    }).catch(err => {
-        res.status(500).json({error: err, message: 'Couldn\'t get users', step: 'api/users/'});
-    })
-})
+router.get("/", validateIdToken, superRoleIdAuth, (req, res) => {
+	Users.find()
+		.then((users) => {
+			users = users.map((user) => dbToRes(user));
+			res.status(200).json(users);
+		})
+		.catch((err) => {
+			res
+				.status(500)
+				.json({
+					error: err,
+					message: "Couldn't get users",
+					step: "api/users/",
+				});
+		});
+});
 
 module.exports = router;
