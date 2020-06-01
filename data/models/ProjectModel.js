2b46e8457ace1ea470f80a39b3d1ee36ffcb3fc3
Scott Smith <scthsm@outlook.com>
2020-06-01 16:50:40 -0600

    revised tallycounter

--- data/models/ProjectModel.js
+++ data/models/ProjectModel.js
@@ -15,7 +15,8 @@ class ProjectModel extends BaseModel {
             'projects.description',
             db.raw('CASE WHEN jobsheets.completed is 0 THEN FALSE ELSE TRUE END as completed'),
             db.raw('group_concat(jobsheets.user_email) as technicians'),
-            db.raw('group_concat(jobsheets.completed) as tally')
+            db.raw('sum(case when jobsheets.completed <> 0 then 1 else 0 end) finished'),
+            db.raw('count(jobsheets.completed) as total')
           ])
           .groupBy('projects.id').where(filter)
   }
