fbd6c06e503b0094574b3941c4347ee6574e3e84
Scott Smith <scthsm@outlook.com>
2020-06-01 17:33:32 -0600

    technicians property returns Unassigned if null

--- data/models/ProjectModel.js
+++ data/models/ProjectModel.js
@@ -13,7 +13,7 @@ class ProjectModel extends BaseModel {
             'projects.client_id',
             'projects.name',
             'projects.description',
-            db.raw('group_concat(jobsheets.user_email) as technicians'),
+            db.raw('IFNULL(group_concat(jobsheets.user_email),"Unassigned") as technicians'),
             db.raw('(sum(case when jobsheets.completed <> 0 then 1 else 0 end)||"/"||count(jobsheets.completed)) tally'),
             db.raw('CASE WHEN jobsheets.completed is 0 THEN FALSE ELSE TRUE END as completed')
           ])
