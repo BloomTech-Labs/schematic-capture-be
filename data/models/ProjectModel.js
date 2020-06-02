36a4c64b4a9404f3de4d1dd1cfed8c4ba27f2bc6
Scott Smith <scthsm@outlook.com>
2020-06-02 11:59:03 -0600

    Revert "Updated tally again"
    This reverts commit a914d10ae0b5325fd0f4d5c4a77e4c5e920f4ebb.

--- data/models/ProjectModel.js
+++ data/models/ProjectModel.js
@@ -13,9 +13,10 @@ class ProjectModel extends BaseModel {
             'projects.client_id',
             'projects.name',
             'projects.description',
+            db.raw('CASE WHEN jobsheets.completed is 0 THEN FALSE ELSE TRUE END as completed'),
             db.raw('group_concat(jobsheets.user_email) as technicians'),
-            db.raw('(sum(case when jobsheets.completed <> 0 then 1 else 0 end)||"/"||count(jobsheets.completed)) tally'),
-            db.raw('CASE WHEN jobsheets.completed is 0 THEN FALSE ELSE TRUE END as completed')
+            db.raw('sum(case when jobsheets.completed <> 0 then 1 else 0 end) finished'),
+            db.raw('count(jobsheets.completed) as total')
           ])
           .groupBy('projects.id').where(filter)
   }
