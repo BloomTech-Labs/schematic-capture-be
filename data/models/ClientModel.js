c1fbc3b7c65249be1f94d8dc866abba7f2fcc95a
Scott Smith <scthsm@outlook.com>
2020-06-02 11:59:14 -0600

    Revert "q"
    This reverts commit 92646d08c6626eb534ca59713509423343baf2c4.

--- data/models/ClientModel.js
+++ data/models/ClientModel.js
@@ -1,17 +1,9 @@
 const BaseModel = require('./BaseModel');
-const mapper = require('./clientMap');
 const db = require('../dbConfig');
 
 class ClientModel extends BaseModel {
   constructor(table) {
     super(table);
-  }
-
-  find() {
-    return db("clients")
-          .select('clients.*')
-          .select('p.completed')
-          .join('projects as p','p.client_id','clients.id')
   }
 
   findByOrganization(filter) {
